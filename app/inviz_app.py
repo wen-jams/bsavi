322a415fa6ea8dcc01dbabea0b78852393e36f27
wen-jams <jimmywen74@gmail.com>
2023-06-12 06:51:18 -0700

    remove loading of data not in repo

--- app/inviz_app.py
+++ app/inviz_app.py
@@ -241,21 +241,21 @@ classy_CDM_slice = classy_CDM[::500].reset_index(drop=True)
 df_slice = df[::500].reset_index(drop=True)
 
 copts = opts.Curve(width=500, height=400, logx=True, padding=0.1, fontscale=1.1, color=hv.Cycle('GnBu'), bgcolor='#22262F', framewise=True)
-# viz(data=df_slice, myfunction=compute_residuals, myfunction_args=(classy_input_slice, classy_CDM_slice), show_observables=True, latex_dict=params_latex_form, curve_opts=copts).servable()
+viz(data=df_slice, myfunction=compute_residuals, myfunction_args=(classy_input_slice, classy_CDM_slice), show_observables=True, latex_dict=params_latex_form, curve_opts=copts).servable('Fractional IDM')
 
 # this data is a simple collection of points that will be plotted as observables
-interp_df = pd.read_pickle('../data/trey_uvlf/bouwens_2023_data.pkl')
-binned_df = pd.read_pickle('../data/trey_uvlf/bouwens_2023_data_binned.pkl')
-params_df = interp_df[['alphaOutflow', 'alphaStar', 'like', 'timescale', 'velocityOutflow']]
-lumfunc_df = interp_df[['uvlf_Muv', 'uvlf_z10.5', 'uvlf_z12.6', 'uvlf_z8.7']]
-uvlf = {}
-for row in lumfunc_df.index:
-    uvlf[row] = {
-        'z = 10.5': {'uvlf_Muv': lumfunc_df['uvlf_Muv'][row], 'uvlf_z10.5': lumfunc_df['uvlf_z10.5'][row]}, 
-        'z = 12.6': {'uvlf_Muv': lumfunc_df['uvlf_Muv'][row], 'uvlf_z12.6': lumfunc_df['uvlf_z12.6'][row]}, 
-        'z = 8.7': {'uvlf_Muv': lumfunc_df['uvlf_Muv'][row], 'uvlf_z8.7': lumfunc_df['uvlf_z8.7'][row]}, 
-    }
-reshaped_uvlf = pd.DataFrame.from_dict(uvlf, orient='index')
+# interp_df = pd.read_pickle('../data/trey_uvlf/bouwens_2023_data.pkl')
+# binned_df = pd.read_pickle('../data/trey_uvlf/bouwens_2023_data_binned.pkl')
+# params_df = interp_df[['alphaOutflow', 'alphaStar', 'like', 'timescale', 'velocityOutflow']]
+# lumfunc_df = interp_df[['uvlf_Muv', 'uvlf_z10.5', 'uvlf_z12.6', 'uvlf_z8.7']]
+# uvlf = {}
+# for row in lumfunc_df.index:
+#     uvlf[row] = {
+#         'z = 10.5': {'uvlf_Muv': lumfunc_df['uvlf_Muv'][row], 'uvlf_z10.5': lumfunc_df['uvlf_z10.5'][row]}, 
+#         'z = 12.6': {'uvlf_Muv': lumfunc_df['uvlf_Muv'][row], 'uvlf_z12.6': lumfunc_df['uvlf_z12.6'][row]}, 
+#         'z = 8.7': {'uvlf_Muv': lumfunc_df['uvlf_Muv'][row], 'uvlf_z8.7': lumfunc_df['uvlf_z8.7'][row]}, 
+#     }
+# reshaped_uvlf = pd.DataFrame.from_dict(uvlf, orient='index')
 
-uvlf_opts = opts.Curve(ylim=(1e-11, None), logy=True, invert_xaxis=True, color=hv.Cycle('GnBu'), bgcolor='#22262F', height=400, width=500)
-viz(data=params_df, data_observable=reshaped_uvlf, curve_opts=uvlf_opts).servable()
+# uvlf_opts = opts.Curve(ylim=(1e-11, None), logy=True, invert_xaxis=True, color=hv.Cycle('GnBu'), bgcolor='#22262F', height=400, width=500)
+# viz(data=params_df, data_observable=reshaped_uvlf, curve_opts=uvlf_opts).servable()
