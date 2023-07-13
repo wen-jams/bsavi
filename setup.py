e8de0db57386c3041fe2760ba9cc920b19b4f335
wen-jams <jimmywen74@gmail.com>
2023-07-13 16:06:52 -0700

    update setup.py, __init__.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name = "inviz",
-    version = "0.2.2",
+    version = "0.2.3",
     author = "James Wen",
     author_email = "jswen@usc.edu",
     description = ("An interactive visualizer to help explore high-dimensional data and its observables."),
@@ -28,7 +28,7 @@ setuptools.setup(
     install_requires=["holoviews==1.15.4",
                       "panel==0.14.4",
                       "spatialpandas==0.4.7",
-                      "param==1.13.0"
+                      "param==1.13.0",
                       "numpy>=1.20, <1.24",
                       "matplotlib==3.7.1"]
     )
